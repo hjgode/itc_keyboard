5f7f9b045e2db3749b2f4e73bdbdee5ab5be5cce
hjgode <hjgode@gmail.com>
2018-09-29 16:30:59 +0200

    started a multikey example

---
+++ MapF1-F5toMultikeys/keymapping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using ITC_KEYBOARD;
+
+namespace MapF1_F5toMultikeys
+{
+    class keymapping
+    {
+        CUSBkeys.usbKeyStruct[] hwKeys = new CUSBkeys.usbKeyStruct[10]{
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F1_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F2_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F3_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F4_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F5_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F6_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F7_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F8_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F9_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+            new CUSBkeys.cusbKeyStruct(0x07, CUsbKeyTypes.HWkeys.F10_cozumel, CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.NoFlag, CUsbKeyTypes.usbFlagsLow.MultiKeyIndex, 0x00).usbKeyStruct,
+        };
+
+
+        CUSBkeys.usbKeyStructShort defaultKeyStructShort = new CUSBkeys.usbKeyStructShort();
+        CUSBkeys.usbKeyStructShort[][] charSequences= new CUSBkeys.usbKeyStructShort[10][]{
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+            new CUSBkeys.usbKeyStructShort[]{
+            new CUSBkeys.cusbKeyStructShort(CUsbKeyTypes.usbFlagsHigh.NoFlag, CUsbKeyTypes.usbFlagsMid.VKEY,CUsbKeyTypes.usbFlagsLow.NormalKey, 0x00).shortStruct,
+            },
+
+        };
+
+        string[] chrSequences = new string[10];
+        public keymapping()
+        {
+            chrSequences[0] = "26D15000";
+            chrSequences[1] = "26D15000";
+            chrSequences[2] = "26D15000";
+            chrSequences[3] = "26D15000";
+            chrSequences[4] = "26D15000"; //f5
+            chrSequences[5] = "26D2999";
+            chrSequences[6] = "26D2999";
+            chrSequences[7] = "26D2999";
+            chrSequences[8] = "26D2999";
+            chrSequences[9] = "26D2999";
+        }
+    }
+}
---
+++ MapF1-F5toMultikeys/map-to-multikeys.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using ITC_KEYBOARD;
+
+namespace MapF1_F5toMultikeys
+{
+    class map_to_multikeys
+    {
+
+        ITC_KEYBOARD.CUSBkeys cusb = null;
+        byte[] keystomap = new byte[]{
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F1_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F2_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F3_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F4_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F5_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F6_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F7_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F8_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F9_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F10_Key,
+/*                (byte)HardwareKeys.AllKeys.ITC_Standard_F11_Key,
+                (byte)HardwareKeys.AllKeys.ITC_Standard_F11_Key,
+*/            };
+        byte[][] keystomapto = new byte[2][]{
+                    // 26D15000
+                new byte[]{
+                    (byte)VKEY.VK_2, 
+                    (byte)VKEY.VK_6,
+                    (byte)VKEY.VK_D,
+                    (byte)VKEY.VK_1,
+                    (byte)VKEY.VK_5,
+                    (byte)VKEY.VK_0,
+                    (byte)VKEY.VK_0,
+                    (byte)VKEY.VK_0},
+                    // 26D2999
+                new byte[]{(byte)
+                    (byte)VKEY.VK_2, 
+                    (byte)VKEY.VK_6,
+                    (byte)VKEY.VK_D,
+                    (byte)VKEY.VK_2,
+                    (byte)VKEY.VK_9,
+                    (byte)VKEY.VK_9,
+                    (byte)VKEY.VK_9}
+        };
+        USBkeys.usbKeyStruct uKey = null;
+
+        public map_to_multikeys()
+        {
+            try
+            {
+                cusb = new ITC_KEYBOARD.CUSBkeys();
+
+                //the key we will map
+                uKey=new CUSBkeys.usbKeyStruct();
+                uKey.bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+                uKey.bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+                uKey.bFlagLow = CUsbKeyTypes.usbFlagsLow.MultiKeyIndex;
+                uKey.bHID = 0x07;
+
+                int iRes = 0;
+                //for each plane???
+                for (int p = 0; p < cusb.getNumPlanes(); p++)
+                {
+                    //for each multikey
+                    for (int k = 0; k < keystomap.Length; k++)
+                    {
+                        byte[] mKey=keystomapto[k];
+                        //create multiKey
+                        CUSBkeys.usbKeyStructShort[] multiKey = new CUSBkeys.usbKeyStructShort[mKey.Length];
+                        for (int x = 0; x < mKey.Length; x++ )
+                        {
+                            multiKey[x].bFlagHigh=CUsbKeyTypes.usbFlagsHigh.NoFlag;
+                            multiKey[x].bFlagMid = CUsbKeyTypes.usbFlagsMid.VKEY;
+                            multiKey[x].bFlagLow = CUsbKeyTypes.usbFlagsLow.NormalKey;
+                            multiKey[x].bIntScan = mKey[x];
+                            //find multikey
+                            //try to find existing MultiKey
+                            CMultiKeys cmulti = new CMultiKeys();
+                            int iMax = cmulti.getMultiKeyCount();
+                            int iFoundMultiKey = cmulti.findMultiKey(multiKey);
+                            if (iFoundMultiKey == -1)//if there was no existing entry, create a new one
+                            {
+                                iFoundMultiKey = cmulti.addMultiKey(uKey);
+                            }
+
+                            uKey.bIntScan = iFoundMultiKey;
+                            uKey.bScanKey = (CUsbKeyTypes.HWkeys)keystomap[k];
+                            iRes = cusb.setKey((int)p, keystomap[k], uKey);
+                            System.Diagnostics.Debug.WriteLine("keymap " + p.ToString() + "/" + k.ToString() + "/" + iRes.ToString());
+                        }
+                    }
+                }
+                iRes = cusb.writeKeyTables();            
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
