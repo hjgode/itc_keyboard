efe520f936eb36f253305a21e14942e288fb5de4
hjgode <hjgode@gmail.com>
2019-08-08 09:53:19 +0200

    updated examples

---
+++ DumpKeys/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DumpKeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(240, 268);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.MenuItems.Add(this.menuItem3);
+            this.menuItem1.Text = "File";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.MenuItems.Add(this.menuItem4);
+            this.menuItem2.MenuItems.Add(this.menuItem5);
+            this.menuItem2.Text = "Options";
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Text = "Save Dump";
+            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Text = "Restore Defaults";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Text = "Refresh Dump";
+            this.menuItem5.Click += new System.EventHandler(this.menuItem5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.textBox1);
+            this.Menu = this.mainMenu1;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.MenuItem menuItem5;
+    }
+}
+
---
+++ DumpKeys/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using ITC_KEYBOARD;
+
+namespace DumpKeys
+{
+    public partial class Form1 : Form
+    {
+        ITC_KEYBOARD.CUSBkeys cusb;
+        public Form1()
+        {
+            InitializeComponent();
+            cusb = new ITC_KEYBOARD.CUSBkeys();
+            readDump();
+        }
+
+        void readDump()
+        {
+            StringBuilder sb= cusb.dumpAllKeys();
+            textBox1.Text = sb.ToString();
+            
+        }
+        private void menuItem3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                using (System.IO.TextWriter sw = new System.IO.StreamWriter(dlg.FileName) )
+                {
+                    sw.WriteLine(textBox1.Text);
+                    sw.Flush();                    
+                }
+            }
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure", "Restore default keyboard", MessageBoxButtons.OKCancel, MessageBoxIcon.Question,MessageBoxDefaultButton.Button1)==DialogResult.OK){
+                cusb.resetKeyDefaults();                
+            }
+        }
+
+        private void menuItem5_Click(object sender, EventArgs e)
+        {
+            readDump();
+        }
+    }
+}
